fa837f09e1123eced56c164216099eb693a40638
Uverlink <marc.pizzanelli@gmail.com>
2022-09-21 20:10:04 +0200

    Stop

--- pages/index.js
+++ pages/index.js
@@ -7,26 +7,28 @@ import { useRouter } from "next/router";
 export default function PaginationPage(props) {
   const router = useRouter();
   const [page, setPage] = useState(1);
-  const { data } = useQuery(
+  const { data:datagame } = useQuery(
     ["games", page],
     async () =>
       await fetch(
-        `https://api.rawg.io/api/games?key=4291ed9152f84825ab9a062319652c80&page=${page}`
+        `https://api.rawg.io/api/games?key=4291ed9152f84825ab9a062319652c80&page=${page}&genre=`
       ).then((result) => result.json()),
     {
       keepPreviousData: true,
     }
   );
-  const { datagenre } = useQuery(
+
+  const { data:datagenre } = useQuery(
     ["genres", page],
     async () =>
-      await fetch(
-        `https://api.rawg.io/api/genres?key=4291ed9152f84825ab9a062319652c80&page=${page}`
-      ).then((result) => result.json()),
+      await fetch('https://api.rawg.io/api/genres?key=4291ed9152f84825ab9a062319652c80').then((result) => 
+         result.json()
+      ),
     {
       keepPreviousData: true,
     }
   );
+
   function handlePaginationChange(e, value) {
     setPage(value);
     router.push(`?page=${value}`, undefined, { shallow: true });
@@ -36,26 +38,30 @@ export default function PaginationPage(props) {
       setPage(parseInt(router.query.page));
     }
   }, [router.query.page]);
+
   return (
     <div>
       <h1>
         GG Commerce
       </h1>
-      <div className='grid-container'>
+      <div className='genre-container'>
         {datagenre?.results?.map((genre) => (
           <article key={genre.id}>
-            <img
-              src={genre.background_image}
-              alt={genre.name}/>
+            {<img
+              src={genre.image_background}
+              alt={genre.name}
+              loading='lazy'
+              width={"100%"}
+              height={"100%"}
+              />}
             <div className='text'>
               <p>{genre.name}</p>
-              <p>{genre.slug}</p>
             </div>
           </article>
         ))}
       </div>
       <Pagination
-        count={data?.count}
+        count={datagame?.count}
         variant='outlined'
         color='standard'
         className='pagination'
@@ -63,7 +69,7 @@ export default function PaginationPage(props) {
         onChange={handlePaginationChange}
       />
       <div className='grid-container'>
-        {data?.results?.map((game) => (
+        {datagame?.results?.map((game) => (
           <article key={game.id}>
             <img
               src={game.background_image}
@@ -79,7 +85,7 @@ export default function PaginationPage(props) {
         ))}
       </div>
       <Pagination
-        count={data?.count}
+        count={datagame?.count}
         variant='outlined'
         color='dcd8c0'
         className='pagination'
